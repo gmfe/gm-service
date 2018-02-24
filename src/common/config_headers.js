d18a321626d74392179ccf36df011a9bf9ee9dde
maltose <liyatang@xiaonongnv.com>
2018-02-24 15:11:55 +0800

    config_headers

---
+++ src/common/config_headers.js
@@ -0,0 +1,25 @@
+import {md5, UUID, RequestInterceptor} from 'gm-util';
+
+const CLIENTIDKEY = '_GM_SERVICE_CLIENT_ID';
+const {localStorage} = window;
+
+let clientId = localStorage.getItem(CLIENTIDKEY) || '';
+if (!clientId) {
+    clientId = md5(UUID.generate());
+    localStorage.setItem(CLIENTIDKEY, clientId);
+}
+
+const configHeaders = (name, version) => {
+    RequestInterceptor.add({
+        request(config) {
+            config.options.headers = config.options.headers || {};
+
+            config.options.headers['X-Guanmai-Client'] = `${name}/${version} ${clientId}`;
+            config.options.headers['X-Guanmai-Request-Id'] = md5(UUID.generate());
+
+            return config;
+        }
+    });
+};
+
+export default configHeaders;
