8a1fae187c70e14b55c217ea3c24875a0712c657
HecateDK <minyillee@gmail.com>
2018-03-13 11:11:25 +0800

    asyncLoadJS 优化

--- src/async_load_js/index.js
+++ src/async_load_js/index.js
@@ -1,46 +1,20 @@
+import _ from 'lodash';
 import { loadScript } from 'gm-util';
 
-const config = {
-    'xlsx': {
-        depUrl: '//js.guanmai.cn/build/libs/node_modules/xlsx/dist/xlsx.full.min.js',
-        isReady: false
-    },
-    'echarts': {
-        depUrl: '//js.guanmai.cn/build/libs/echarts/4.0.4/dist/echarts.min.js',
-        isReady: false
-    },
-    'baidumapApi': {
-        depUrl: '//api.map.baidu.com/api?v=2.0&ak=uRIgQnOKFQ77LLvuI9WzNgri',
-        isReady: false
-    },
-    'baidumapLushu': {
-        depUrl: '//api.map.baidu.com/library/LuShu/1.2/src/LuShu_min.js',
-        isReady: false
-    },
-    'pdfmake': {
-        depUrl: '//js.guanmai.cn/build/libs/node_modules/gm-pdfmake/build/pdfmake.min.js?v=0.2.16',
-        isReady: false
-    },
-    'pdfmakeRegular0': {
-        depUrl: '//js.guanmai.cn/build/libs/node_modules/gm-pdfmake/build/splits/regular-0.js?v=0.2.16',
-        isReady: false
-    },
-    'pdfmakeRegular1': {
-        depUrl: '//js.guanmai.cn/build/libs/node_modules/gm-pdfmake/build/splits/regular-1.js?v=0.2.16',
-        isReady: false
-    },
-    'pdfmakeBold0': {
-        depUrl: '//js.guanmai.cn/build/libs/node_modules/gm-pdfmake/build/splits/bold-0.js?v=0.2.16',
-        isReady: false
-    },
-    'pdfmakeBold1': {
-        depUrl: '//js.guanmai.cn/build/libs/node_modules/gm-pdfmake/build/splits/bold-1.js?v=0.2.16',
-        isReady: false
-    }
-};
+const config = [
+    {name: 'xlsx', depUrl: '//js.guanmai.cn/build/libs/node_modules/xlsx/dist/xlsx.full.min.js'},
+    {name: 'echarts', depUrl: '//js.guanmai.cn/build/libs/echarts/4.0.4/dist/echarts.min.js'},
+    {name: 'baidumapApi', depUrl: '//api.map.baidu.com/api?v=2.0&ak=uRIgQnOKFQ77LLvuI9WzNgri'},
+    {name: 'baidumapLushu', depUrl: '//api.map.baidu.com/library/LuShu/1.2/src/LuShu_min.js'},
+    {name: 'pdfmake', depUrl: '//js.guanmai.cn/build/libs/node_modules/gm-pdfmake/build/pdfmake.min.js?v=0.2.16'},
+    {name: 'pdfmakeRegular0', depUrl: '//js.guanmai.cn/build/libs/node_modules/gm-pdfmake/build/splits/regular-0.js?v=0.2.16'},
+    {name: 'pdfmakeRegular1', depUrl: '//js.guanmai.cn/build/libs/node_modules/gm-pdfmake/build/splits/regular-1.js?v=0.2.16'},
+    {name: 'pdfmakeBold0', depUrl: '//js.guanmai.cn/build/libs/node_modules/gm-pdfmake/build/splits/bold-0.js?v=0.2.16'},
+    {name: 'pdfmakeBold1', depUrl: '//js.guanmai.cn/build/libs/node_modules/gm-pdfmake/build/splits/bold-1.js?v=0.2.16'}
+];
 
-const asyncLoadJS = function(conf, callback){
-    const target = config[conf];
+const asyncLoadJS = (conf, callback) =>  {
+    const target = _.find(config, val => val.name === conf);
 
     if (!target) {
         console.error('模块不存在');
