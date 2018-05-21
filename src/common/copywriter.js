b847362479e5a4f6850f76d822bb6e71ec2a559c
HecateDK <minyillee@gmail.com>
2018-05-21 11:56:44 +0800

    delete copywriterByTaxRate

--- src/common/copywriter.js
+++ src/common/copywriter.js
@@ -1,11 +1,3 @@
-// 根据有没有查看税率权限get_tax，判断显示什么文案
-function copywriterByTaxRate(notax, hastax) {
-    if( viewTaxRate ) {
-        return hastax;
-    }
-    return notax;
-}
-
 // 12 -> SC000012 1232131 ->SC1232131
 const idConvert2Show = (id, prefix) => {
     let convertId = window.parseInt(id, 10);
@@ -16,10 +8,6 @@ const idConvert2Show = (id, prefix) => {
     }
 };
 
-
-
-
 export {
-    copywriterByTaxRate,
     idConvert2Show
 };
