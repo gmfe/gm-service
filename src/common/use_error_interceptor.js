73b1ec33d1230302444da1c216f3938f8b716cd4
yiyong <yiyong@guanmai.cn>
2018-11-21 10:55:10 +0800

    报错提示去掉 url

--- src/common/use_error_interceptor.js
+++ src/common/use_error_interceptor.js
@@ -6,14 +6,14 @@ const useErrorInterceptor = () => {
   const showMsg = (shower, msgs) => {
     shower({
       children: msgs.map((msg, i) => <div key={i}> {msg} </div>),
-      time: 0 // 不消失
+      time: 5000
     })
   }
   const errorInterceptor = {
     response (json, config) {
       if (!config.sucCode.includes(json.code)) {
         // 业务错误
-        let msg = [json.msg || '未知的错误', config.url]
+        let msg = [json.msg || '未知的错误']
         showMsg(Tip.warning.bind(Tip), msg)
       }
       return json
@@ -22,7 +22,7 @@ const useErrorInterceptor = () => {
       // reason 两种
       // 1. http错误 502 Bad Gateway 等
       // 2. 连接超时 TypeError Failed to fetch 等网络问题
-      let msg = [reason + '', config.url]
+      let msg = [reason + '']
       showMsg(Tip.danger.bind(Tip), msg)
     }
   }
